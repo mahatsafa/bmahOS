7b86a7dd53efaca618bccf21b3d20efa7c74168f
mahatsafa <bimbimgus@gmail.com>
2026-08-11 16:44:15 +0700

    Implement bmahOS GDT transition

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -68,14 +68,177 @@ static void serial_write_hex(uint64_t value)
     }
 }
 
+
+struct gdt_entry
+{
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t  base_middle;
+    uint8_t  access;
+    uint8_t  granularity;
+    uint8_t  base_high;
+} __attribute__((packed));
+
+static struct gdt_entry bmahOS_gdt[3];
+
+static void gdt_set_entry(
+    int index,
+    uint8_t access,
+    uint8_t granularity
+)
+{
+    bmahOS_gdt[index].limit_low = 0x0000;
+    bmahOS_gdt[index].base_low = 0x0000;
+    bmahOS_gdt[index].base_middle = 0x00;
+    bmahOS_gdt[index].access = access;
+    bmahOS_gdt[index].granularity = granularity;
+    bmahOS_gdt[index].base_high = 0x00;
+}
+
+static void gdt_init(void)
+{
+    gdt_set_entry(0, 0x00, 0x00);
+    gdt_set_entry(1, 0x9B, 0x20);
+    gdt_set_entry(2, 0x93, 0x00);
+}
+
+static void print_bmahOS_gdt(void)
+{
+    serial_write("bmahOS GDT entries:\r\n");
+
+    for (uint64_t i = 0; i < 3; i++)
+    {
+        uint64_t descriptor =
+            *(volatile uint64_t *)&bmahOS_gdt[i];
+
+        serial_write("bmahOS_GDT[");
+        serial_write_hex(i);
+        serial_write("] = ");
+        serial_write_hex(descriptor);
+        serial_write("\r\n");
+    }
+}
+
+extern void gdt_load_and_reload_asm(const void *gdtr);
+
+static void gdt_load_and_reload(void)
+{
+    struct {
+        uint16_t limit;
+        uint64_t base;
+    } __attribute__((packed)) gdtr;
+
+    gdtr.limit = sizeof(bmahOS_gdt) - 1;
+    gdtr.base = (uint64_t)&bmahOS_gdt[0];
+
+    gdt_load_and_reload_asm(&gdtr);
+}
+
+static void read_bmahOS_gdtr(void)
+{
+    struct {
+        uint16_t limit;
+        uint64_t base;
+    } __attribute__((packed)) gdtr;
+
+    gdtr.limit = sizeof(bmahOS_gdt) - 1;
+    gdtr.base = (uint64_t)&bmahOS_gdt[0];
+
+    serial_write("bmahOS GDT limit: ");
+    serial_write_hex(gdtr.limit);
+    serial_write("\r\n");
+
+    serial_write("bmahOS GDT base: ");
+    serial_write_hex(gdtr.base);
+    serial_write("\r\n");
+}
+
+static void read_segment_registers(void)
+{
+    uint16_t cs;
+    uint16_t ds;
+    uint16_t ss;
+
+    __asm__ volatile ("mov %%cs, %0" : "=r"(cs));
+    __asm__ volatile ("mov %%ds, %0" : "=r"(ds));
+    __asm__ volatile ("mov %%ss, %0" : "=r"(ss));
+
+    serial_write("CS: ");
+    serial_write_hex(cs);
+    serial_write("\r\n");
+
+    serial_write("DS: ");
+    serial_write_hex(ds);
+    serial_write("\r\n");
+
+    serial_write("SS: ");
+    serial_write_hex(ss);
+    serial_write("\r\n");
+}
+
+
+static void read_gdtr(void)
+{
+    struct {
+        uint16_t limit;
+        uint64_t base;
+    } __attribute__((packed)) gdtr;
+
+    __asm__ volatile ("sgdt %0" : "=m"(gdtr));
+
+    serial_write("GDT limit: ");
+    serial_write_hex(gdtr.limit);
+    serial_write("\r\n");
+
+    serial_write("GDT base: ");
+    serial_write_hex(gdtr.base);
+    serial_write("\r\n");
+}
+
+static void read_gdt_entries(void)
+{
+    struct {
+        uint16_t limit;
+        uint64_t base;
+    } __attribute__((packed)) gdtr;
+
+    __asm__ volatile ("sgdt %0" : "=m"(gdtr));
+
+    uint64_t entries = (gdtr.limit + 1) / 8;
+
+    serial_write("GDT entries:\r\n");
+
+    for (uint64_t i = 0; i < entries; i++)
+    {
+        uint64_t descriptor =
+            *(volatile uint64_t *)(gdtr.base + (i * 8));
+
+        serial_write("GDT[");
+        serial_write_hex(i);
+        serial_write("] = ");
+        serial_write_hex(descriptor);
+        serial_write("\r\n");
+    }
+}
+
 void kmain(void)
 {
     serial_init();
+
+    gdt_init();
+    gdt_load_and_reload();
 
     serial_write("bmahOS booted!\r\n");
     serial_write("kernel: ");
     serial_write_hex(0xFFFFFFFF80000000ULL);
     serial_write("\r\n");
+
+    read_segment_registers();
+    read_gdtr();
+    read_gdt_entries();
+
+    read_bmahOS_gdtr();
+    print_bmahOS_gdt();
 
     for (;;) {
         __asm__ volatile ("hlt");
